15b8e842e12cb8d20a6af21aa0359a28ceda2886
sateeshgana <41966383+sateeshgana@users.noreply.github.com>
2021-09-01 20:59:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-
+var cities = [{key:1, locaion:"Hyderabad - Banjara Hills"}, {key:2, locaion:"Hyderabad - Nampally", {key:3, locaion:"Hyderbad - Hitech City"}}];
+var key = cities.filter(x => x.key == abc);
+$("input[name='menu-963']").val(key[0].key);
