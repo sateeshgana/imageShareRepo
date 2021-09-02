bb64abab43367c4dca7b7f8b5d185062e181e066
sateeshgana <41966383+sateeshgana@users.noreply.github.com>
2021-09-02 11:40:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var cities = [{key:1, locaion:"Hyderabad - Banjara Hills"}, {key:2, locaion:"Hyderabad - Nampally"}, {key:3, locaion:"Hyderbad - Hitech City"}];
 var key = cities.filter(x => x.key == abc);
-$("select[name='menu-963'] option[value="+key[0].location+"]").prop("selected", true);
+$("[name='menu-963'] option[value="+key[0].location+"]").prop("selected", true);
